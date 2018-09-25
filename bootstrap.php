3332b01e1fd714600374c43d24fa0b6dc84b42a9
JC Camargo <jccamargo15@gmail.com>
2018-09-25 20:06:36 -0300

    primeiro commit

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__ . '/src/resolve-route.php';
+require __DIR__ . '/src/render.php';
+require __DIR__ . '/src/connection.php';
+
+if (resolve('/admin/?(.*)')) {
+    require __DIR__ . '/admin/routes.php';
+} elseif (resolve('/(.*)')) {
+    require __DIR__ . '/site/routes.php';
+}
---
+++ src/connection.php
@@ -0,0 +1,4 @@
+<?php
+
+mysqli_report(MYSQLI_REPORT_ERROR);
+$conn = new mysqli('localhost', 'root', '', 'test');
